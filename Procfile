4b55dd3ca8993a4762119281e39730adad2c99af
shivam kohli <kohlishivam5522@gmail.com>
2017-06-12 12:35:51 +0530

    first commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn emoji.wsgi
---
+++ htMusicBot/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+import main.views as v
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'htMusicBot.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^index$',v.index),
+    url(r'^facebook_auth/?$',v.MyChatBotView.as_view()),
+)
