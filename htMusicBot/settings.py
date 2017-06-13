d9dffd99d39a8140d90f1ebc5c554ac089a3b075
vishrutkohli <kohlivishrut@gmail.com>
2017-06-13 16:46:19 +0530

    a

--- htMusicBot/settings.py
+++ htMusicBot/settings.py
@@ -64,8 +64,12 @@ GRAPH_MODELS = {
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'HTmusicbot',
+        'USER': 'HTmusicbot',
+        'PASSWORD': 'hindustantimes',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
