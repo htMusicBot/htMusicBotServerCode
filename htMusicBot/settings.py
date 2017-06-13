047e06140a6600ebdebb46efdaa127bed1fb349a
vishrutkohli <kohlivishrut@gmail.com>
2017-06-13 17:23:06 +0530

    a

--- htMusicBot/settings.py
+++ htMusicBot/settings.py
@@ -62,6 +62,10 @@ GRAPH_MODELS = {
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
