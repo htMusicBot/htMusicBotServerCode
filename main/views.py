633a798d54a26f26b276da8f1d518154967965f0
shivam kohli <kohlishivam5522@gmail.com>
2017-06-15 12:36:42 +0530

    a

--- main/views.py
+++ main/views.py
@@ -143,9 +143,9 @@ class MyChatBotView(generic.View):
                             userInstance.MovieName = item
                         # userInstance.Singer.add(a[0])
                         userInstance.save()
-                        c = random.shuffle(b)
+                        # c = random.shuffle(b)
                         # post_facebook_message(sender_id,b[0].SongName)
-                        post_facebook_message(sender_id,c[0].SongName)
+                        post_facebook_message(sender_id,b[0].SongName)
                         post_facebook_message(sender_id,'singerQuickreply')
 
 
@@ -171,9 +171,9 @@ class MyChatBotView(generic.View):
                             userInstance.Category.add(item)
                         # userInstance.Singer.add(a[0])
                         userInstance.save()
-                        c = random.shuffle(b)
+                        # c = random.shuffle(b)
                         # post_facebook_message(sender_id,b[0].SongName)
-                        post_facebook_message(sender_id,c[0].SongName)
+                        post_facebook_message(sender_id,b[0].SongName)
                         post_facebook_message(sender_id,'singerQuickreply')
 
                     elif userInstance.State=='year':
