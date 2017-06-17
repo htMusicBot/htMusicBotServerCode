80abf162182944c2abec42a03b2885f8ce7eeab0
vishrutkohli <kohlivishrut@gmail.com>
2017-06-18 03:14:07 +0530

    a

--- main/views.py
+++ main/views.py
@@ -763,14 +763,14 @@ def SongSearcher(sender_id):
         card_data = {
                   "title": i.SongName,
                   "subtitle": singerNames,
-                  "image_url": "http://www.mv.dhl.com/content/dam/DHL_Express/Marketing_Stage/dhl_express_720x233.jpg",
+                  "image_url": song_img,
                   
                   "buttons": [
                   {
                     "type":"web_url",
-                    # "url":i.YoutubeLink
+                    "url":i.YoutubeLink
 
-                    "url":"https://scontent.fdel8-1.fna.fbcdn.net/v/t34.0-12/19264885_1537111976319038_153011396_n.png?oh=754c80143d667a42a58350b5162f83ba&oe=59473531",
+                    # "url":"https://scontent.fdel8-1.fna.fbcdn.net/v/t34.0-12/19264885_1537111976319038_153011396_n.png?oh=754c80143d667a42a58350b5162f83ba&oe=59473531",
                     "title":"Play song",
                     "webview_height_ratio": "compact"
                   } ,
