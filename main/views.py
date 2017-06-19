07fbbf9634cbdc07104f1cdcfa77fbe46cb3d8b2
vishrutkohli <kohlivishrut@gmail.com>
2017-06-19 10:49:07 +0530

    a

--- main/views.py
+++ main/views.py
@@ -768,7 +768,7 @@ def SongSearcher(sender_id):
                   "buttons": [
                   {
                     "type":"web_url",
-                    "url":i.YoutubeLink
+                    "url":i.YoutubeLink,
 
                     # "url":"https://scontent.fdel8-1.fna.fbcdn.net/v/t34.0-12/19264885_1537111976319038_153011396_n.png?oh=754c80143d667a42a58350b5162f83ba&oe=59473531",
                     "title":"Play song",
