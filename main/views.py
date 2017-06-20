fcd36d3db7f98d02d804970d08221a08ef417bc0
vishrutkohli <kohlivishrut@gmail.com>
2017-06-20 14:51:37 +0530

    loop error debug

--- main/views.py
+++ main/views.py
@@ -1188,6 +1188,194 @@ def greetingButton():
 
 
 
+def Category_quickreplies(sender_id):
+    userInstance = UserData.objects.get_or_create(Fbid =sender_id)[0]
+
+    arraySinger =[]
+    arrayYear =[]
+    arrayCategory =[]
+    arrayActor =[]
+    arrayLyricist =[]
+    arrayMovie =[]
+
+    for item in userInstance.Singer.all():
+        arraySinger.append(item.Name)
+
+    # for item in userInstance.year.all():
+    #     arrayYear.append(item.Name)
+        
+    for item in userInstance.Category.all():
+        arrayCategory.append(item.Name)
+        
+    for item in userInstance.Cast.all():
+        arrayActor.append(item.Name)
+        
+    for item in userInstance.Lyricist.all():
+        arrayLyricist.append(item.Name)                  
+
+    if userInstance.year:
+        arrayYear.append(userInstance.year)
+    else:
+        pass    
+
+    if userInstance.MovieName:
+        arrayMovie.append(userInstance.MovieName)
+        print  "Im if loop  = " + str(arrayMovie)
+
+
+    else:
+        pass          
+
+    print "arrays of all parameters made"
+
+
+
+    q = Singer.objects.filter(Name__in = arraySinger)
+
+    print "entered singer "
+    print "haha" + str(q)
+    w = Year.objects.filter(Year = userInstance.year)
+    print w
+    print "entered year "
+    y = MovieName.objects.filter(Name = userInstance.MovieName)
+    print y
+    print "entered movie "
+    e = Category.objects.filter(Name__in = arrayCategory)
+    print e
+    print "entered category "
+
+    r = Actor.objects.filter(Name__in = arrayActor)
+    print r
+    print "entered actor "
+    t = Lyricist.objects.filter(Name__in = arrayLyricist)
+    print t
+    print "entered lyricist "
+
+    if arraySinger:
+
+        b = Song.objects.filter(Singer=q) 
+
+
+    else :
+        b =  Song.objects.exclude(Singer=q)
+
+    print "After sorting singers" 
+    print b  
+
+    if arrayYear:
+        print "yes in array year"
+
+        z = b.filter(year=w) 
+
+
+    else :
+        z =  b.exclude(year=w)
+
+    print "After sorting years"     
+    print z    
+
+    # if arrayCategory:
+
+    #     h = z.filter(Category=e) 
+
+
+    # else :
+    #     h =  z.exclude(Category=e)
+
+    print "After sorting category" 
+    print h     
+
+    if arrayActor :
+
+        i = z.filter(Cast=r) 
+
+
+    else :
+        i =  z.exclude(Cast=r)
+
+
+    print "After sorting actor"    
+
+    print i     
+
+    if arrayLyricist :
+
+        a = i.filter(Lyricist=t) 
+
+
+    else :
+        a =  i.exclude(Lyricist=t) 
+
+    print "After sorting Lyricist"     
+
+    print a    
+
+    if arrayMovie :
+
+        c = a.filter(MovieName=y) 
+
+
+    else :
+        c =  a.exclude(MovieName=y)  
+
+    print "After sorting Movie"      
+
+    print c         
+
+
+
+
+    print "best best " + str(c)
+
+    card_data2 = []
+    print c 
+    number = 0
+    for i in c:
+
+        number = number + 1
+        print number
+        print "entered loop"
+        y = i.YoutubeLink
+        # arraySinger = []
+        x = y.split("/")
+        print "x = " + str(x)
+        song_img = "https://img.youtube.com/vi/" + x[-1] + "/hqdefault.jpg"
+        singerNames = ''
+        for item in i.Category.all():
+            
+
+
+        
+        
+            card_data ={
+                                    "content_type":"text",
+                                    "title":item.Name,
+                                    "payload":item.Name
+                                  }
+
+        card_data2.append(card_data) 
+        print "cards appended"   
+        if number == 10:
+            break       
+
+                    
+    response_object =  {
+                            "recipient":{
+                              "id":sender_id
+                          },
+                          "message":{
+                            "text":"you can choose any one of these categories?",
+                            "quick_replies":quickreply_array
+                          }
+                        }
+
+    print "response dumped"
+
+    print json.dumps(response_object)
+
+    # print response_object
+
+    return json.dumps(response_object)
 
 
 
