6cecdf98374c6a4648119b0eecf4584fcd6fd9e2
shivam kohli <kohlishivam5522@gmail.com>
2017-06-20 14:32:10 +0530

    a

--- main/views.py
+++ main/views.py
@@ -926,8 +926,8 @@ def matching_quickreplies(input_string , data , sender_id) :
     w =0
     for i in range(3):
 
-        if max(a)>0.5:
-            print "this is max ratio" + str(a.index(max(a)))
+        if max(a)>50:
+            print "this is max ratio" + str(max(a))
 
             match = data[a.index(max(a))].Name
             
