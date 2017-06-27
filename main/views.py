03ae0601c61bb633fd1ca453508c2f25f43264e2
vishrutkohli <kohlivishrut@gmail.com>
2017-06-27 16:06:52 +0530

    checking query

--- main/views.py
+++ main/views.py
@@ -659,7 +659,8 @@ def handle_quickreply(fbid,payload):
 
 
         post_facebook_message(sender_id,'moreSongs')
-        return post_facebook_message(sender_id,'ACards')
+        
+        post_facebook_message(sender_id,'ACards')
 
     elif payload == 'filter':
         p = UserData.objects.get_or_create(Fbid =fbid)[0]
