ae5705ec764d777331f17cedda94f1fd103a441e
shivam kohli <kohlishivam5522@gmail.com>
2017-06-27 15:33:04 +0530

    adding text template

--- main/views.py
+++ main/views.py
@@ -81,6 +81,7 @@ def post_facebook_message(fbid,message_text):
                     headers={"Content-Type": "application/json"},
                     data=response_msg)
 
+
 def post_matching_quickreplies(fbid,message_text , data , input_string):
     post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token=%s'%PAGE_ACCESS_TOKEN
 
@@ -654,9 +655,6 @@ def handle_quickreply(fbid,payload):
         p.State = 'NULL'
         p.save()
         post_facebook_message(sender_id,'cards')
-        afterOptionText = ['Do you want to hear more songs like this? Choose from these options' , 'What more can I play for you? Select options' , 'Add more filters to narrow down your search or start over.']
-        a = random.choice(afterOptionText)
-        post_facebook_message(sender_id,str(a)) 
         post_facebook_message(sender_id,'moreSongs')
         return post_facebook_message(sender_id,'ACards')
 
@@ -672,9 +670,6 @@ def handle_quickreply(fbid,payload):
     elif payload == 'reset':
         p = UserData.objects.get_or_create(Fbid =fbid)[0]
         p.delete()
-        startOverText = ['Want to listen to something different? Choose from the options below' , 'Tell me what you want to hear now' , 'I can play something different for you. Help me by choosing from the options below']
-        a = random.choice(startOverText)
-        post_facebook_message(sender_id,str(a))
         return post_facebook_message(sender_id,'singerQuickreply')
 
 
@@ -733,7 +728,9 @@ def singerQuickreply(fbid):
 
 
 def afterSongQuickreply(fbid):
-    
+    afterOptionText = ['Do you want to hear more songs like this? Choose from these options' , 'What more can I play for you? Select options' , 'Add more filters to narrow down your search or start over.','Want to listen to something different? Choose from the options below' , 'Tell me what you want to hear now' , 'I can play something different for you. Help me by choosing from the options below']
+    a = random.choice(afterOptionText)
+    post_facebook_message(sender_id,str(a)) 
     response_object =   {
                           "recipient":{
                             "id":fbid
