cd022200457423c14c1ec24754a5e4ec990a0560
shivam kohli <kohlishivam5522@gmail.com>
2017-06-27 14:13:58 +0530

    adding text template

--- main/views.py
+++ main/views.py
@@ -147,19 +147,19 @@ class MyChatBotView(generic.View):
                     sender_id = message['sender']['id']
                     message_text = message['message']['text']
                     DataInstance = userdeatils(sender_id)
-                    name = '%s'%(DataInstance['first_name'])
+                    firstName = '%s'%(DataInstance['first_name'])
                     userInstance = UserData.objects.get_or_create(Fbid =sender_id)[0]
 
 
                     if message_text.lower() in "hey,hi,supp,hello".split(','):
                         print "entered in hi "
                         #messages sent when any user sends the first message
-                        greetingTextTemplate = ['Welcome %s , Nice to see you here :)'%name , 'Hey %s , Welcome to the Music Bot by Hindustan Times :)'%name , 'Hey %s ! Get ready for some Bollywood nostalgia.'%name , 'Hi %s , here is your one-stop destination for Bollywood music. '%name, 'Hello, %s . In the mood for some Bollywood tunes?'%name , 'Hi %s , welcome to HT’s Music Bot. I have Bollywood tunes for you to brighten the day. '%name ]
+                        textTemplate = ['Welcome %s , Nice to see you here :)'%firstName , 'Hey %s , Welcome to the Music Bot by Hindustan Times :)'%name , 'Hey %s ! Get ready for some Bollywood nostalgia.'%name , 'Hi %s , here is your one-stop destination for Bollywood music. '%name, 'Hello, %s . In the mood for some Bollywood tunes?'%name , 'Hi %s , welcome to HT’s Music Bot. I have Bollywood tunes for you to brighten the day. '%name ]
                         # greetingTextTemplate = random.shuffle(greetingTextTemplate)
                         # text = greetingTextTemplate[0].decode('utf-8')
                         # print text
-                        print greetingTextTemplate[0]
-                        post_facebook_message(sender_id , str(greetingTextTemplate[0]) )
+                        print textTemplate[0]
+                        post_facebook_message(sender_id , str(textTemplate[0]) )
                         userInstance.delete()
                         # post_facebook_message(sender_id , 'send us your craving in the following format and we will serve you the best we can . ')
                         # post_facebook_message(sender_id,'#Songname *Singers $Actorsinsong !yourmood')
