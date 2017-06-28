eee201370767507feb7ef403a0f9efeadfdb9379
shivam kohli <kohlishivam5522@gmail.com>
2017-06-28 13:46:59 +0530

    filter

--- main/views.py
+++ main/views.py
@@ -1057,11 +1057,7 @@ def SongSearcher(sender_id):
         post_facebook_message(sender_id,str(options))
     
     
-        post_facebook_message(sender_id,a)
-        post_facebook_message(sender_id,b)
-        post_facebook_message(sender_id,c)
-        post_facebook_message(sender_id,d)
-        post_facebook_message(sender_id,e)
+        post_facebook_message(sender_id,optionSelected[0])
 
         post_facebook_message(sender_id,str(filerOptions))     
 
@@ -1312,11 +1308,8 @@ def songs_cards(sender_id , data , input_string):
     post_facebook_message(sender_id,str(options))
     
     
-    post_facebook_message(sender_id,a)
-    post_facebook_message(sender_id,b)
-    post_facebook_message(sender_id,c)
-    post_facebook_message(sender_id,d)
-    post_facebook_message(sender_id,e)
+    post_facebook_message(sender_id,optionSelected[0])
+    
 
     post_facebook_message(sender_id,str(filerOptions))   
 
@@ -1796,12 +1789,9 @@ def moreSongs(sender_id):
         post_facebook_message(sender_id,str(options))
     
     
-        post_facebook_message(sender_id,a)
-        post_facebook_message(sender_id,b)
-        post_facebook_message(sender_id,c)
-        post_facebook_message(sender_id,d)
-        post_facebook_message(sender_id,e)
+        post_facebook_message(sender_id,optionSelected[0])
 
+        
         post_facebook_message(sender_id,str(filerOptions))   
     
 
