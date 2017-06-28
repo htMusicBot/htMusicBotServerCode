07f77eb485aefc93c401e6ca168c16a2eeb27bb9
vishrutkohli <kohlivishrut@gmail.com>
2017-06-28 18:36:04 +0530

    a

--- main/views.py
+++ main/views.py
@@ -1130,9 +1130,10 @@ def matching_quickreplies(input_string , data , sender_id) :
                 w = w+1
                 print "debugging  " + str(w)
                 print "this is matched quickreply array" + str(quickreply_array)
-                if w==3:
-                break
                 
+                if w==3:
+                    break
+
     else :
         pass         
 
