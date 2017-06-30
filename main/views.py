c971a00aca3fadb1658e7086eed305fc662dadff
shivam kohli <kohlishivam5522@gmail.com>
2017-07-01 00:09:44 +0530

    adding cast

--- main/views.py
+++ main/views.py
@@ -297,7 +297,7 @@ class MyChatBotView(generic.View):
                             message_text = ['1990', '1991', '1992', '1993', '1994', '1995', '1996', '1997', '1998', '1999']
                         elif payload=='2000s':
                             message_text = ['2000','2001', '2002', '2003', '2004', '2005', '2006', '2007', '2008', '2009']
-                        elif payload=='2010':
+                        elif payload=='2010s':
                             message_text = ['2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017']
                         print message_text
                         print 'lalalalala'
