21448335a0ef94b439cef2fb310d7930f32bb11e
vishrutkohli <kohlivishrut@gmail.com>
2017-07-01 18:47:42 +0530

    awesome

--- main/views.py
+++ main/views.py
@@ -1185,7 +1185,7 @@ def matching_quickreplies(input_string , data , sender_id) :
 
                  # s = fuzz.ratio(item.Name, input_string)
                 realName  =item.Name
-                print realName.split()
+                # print realName.split()
                 if len(realName.split())>1
                     s = difflib.SequenceMatcher(None, realName.lower().split()[1], input_string.lower().split()[0]).ratio()
                     a.append(s)
