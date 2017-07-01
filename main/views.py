cfc3d3a3bbaeec2a68246a52afc49be08bdb4186
shivam kohli <kohlishivam5522@gmail.com>
2017-07-01 23:08:21 +0530

    adding cast

--- main/views.py
+++ main/views.py
@@ -1074,7 +1074,7 @@ def SongSearcher(sender_id):
                 year.append(a)
             maxYear = max(year)
             minYear = min(year)
-            f = 'Year: ' + maxYear + '-' + minYear
+            f = str('Year: ' + maxYear + '-' + minYear)
             optionSelected.append(f)
 
 
@@ -1569,7 +1569,7 @@ def songs_cards(sender_id , data , input_string):
             year.append(a)
         maxYear = max(year)
         minYear = min(year)
-        f = 'Year: ' + maxYear + '-' + minYear
+        f = str('Year: ' + maxYear + '-' + minYear)
         optionSelected.append(f)
 
 
@@ -2077,7 +2077,7 @@ def moreSongs(sender_id):
                 year.append(a)
             maxYear = max(year)
             minYear = min(year)
-            f = 'Year: ' + maxYear + '-' + minYear
+            f = str('Year: ' + maxYear + '-' + minYear)
             optionSelected.append(f)
 
         
